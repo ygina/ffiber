0e69228077f2b164325358128491911a62de59a1
Gina Yuan <gina.yuan@gmail.com>
2022-07-27 12:54:05 -0600

    Cornflakes mlx5-datapath example

---
+++ examples/cornflakes.rs
@@ -0,0 +1,48 @@
+use ffiber::CDylibCompiler;
+use ffiber::types::{ArgType, SelfArgType};
+use color_eyre::eyre::Result;
+
+fn main() -> Result<()> {
+    let mut compiler = CDylibCompiler::new("mlx-datapath-c", ".");
+    compiler.add_crate("mlx5-datapath",
+        "{{ path = \"/users/ygina/cornflakes/mlx5-datapath\"}}")?;
+    compiler.add_dependency(
+        "mlx5_datapath::datapath::connection::Mlx5Connection")?;
+    compiler.add_extern_c_function(
+        None,
+        "Mlx5Connection",
+        "set_copying_threshold",
+        Some(SelfArgType::Mut),
+        vec![
+            ("copying_threshold", ArgType::Primitive { ty: "usize".to_string() }),
+        ],
+        None,
+        false,
+    )?;
+    compiler.add_extern_c_function(
+        None,
+        "Mlx5Connection",
+        "add_memory_pool",
+        Some(SelfArgType::Mut),
+        vec![
+            ("buf_size", ArgType::Primitive { ty: "usize".to_string() }),
+            ("min_elts", ArgType::Primitive { ty: "usize".to_string() }),
+        ],
+        None,
+        false,
+    )?;
+    compiler.add_extern_c_function(
+        None,
+        "Mlx5Connection",
+        "add_tx_mempool",
+        Some(SelfArgType::Mut),
+        vec![
+            ("size", ArgType::Primitive { ty: "usize".to_string() }),
+            ("min_elts", ArgType::Primitive { ty: "usize".to_string() }),
+        ],
+        None,
+        false,
+    )?;
+    compiler.flush()?;
+    Ok(())
+}
